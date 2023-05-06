98219076bea1691305a94e2abe5cfff2eb2e794b
Bruno Mota <bmotadev@gmail.com>
2023-05-06 18:06:20 -0300

    :sparkles: feat: Exercicio 01 Concluido

--- desafios/desafio-javascripto/exercicio01/exercicio01.js
+++ desafios/desafio-javascripto/exercicio01/exercicio01.js
@@ -1,7 +1,18 @@
 // 1 - Crie um formulário com 4 botões que, ao clicar em cada um, altere a cor do plano de fundo da página para vermelho,
 // verde, azul e amarelo, respectivamente.
 
+function vermelho() {
+    document.body.style.backgroundColor = "red";
+}
 
-function mudarCor() {
-    document.body.style.backgroundColor = document.getElementById();
+function verde() {
+    document.body.style.backgroundColor = "green";
+}
+
+function azul() {
+    document.body.style.backgroundColor = "blue";
+}
+
+function amarelo() {
+    document.body.style.backgroundColor = "yellow";
 }
