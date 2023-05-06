f04738dbdad1be1255605d457d1d2d40a5e30306
Bruno Mota <bmotadev@gmail.com>
2023-05-06 19:52:51 -0300

    :sparkles: feat: exercicio03 concluido

--- desafios/desafio-javascripto/exercicio03/exercicio03.js
+++ desafios/desafio-javascripto/exercicio03/exercicio03.js
@@ -1,2 +1,9 @@
 // 3 - Crie uma página com uma lista (select - RadioButton) com 4 opções de cores. Ao selecionar uma opção de cor,
 // altere o plano de fundo da página para a cor específica
+
+const fundo = document.querySelector("body");
+
+ function mudarCor(cor) {
+     fundo.style.backgroundColor = cor;
+
+ }
