a884c41de43db5535d378914653bf76dcb491b6d
Bruno Mota <bmotadev@gmail.com>
2023-05-06 18:58:25 -0300

    :recycle: refactor: Alterado exercicio01

--- desafios/desafio-javascripto/exercicio01/exercicio01.js
+++ desafios/desafio-javascripto/exercicio01/exercicio01.js
@@ -1,18 +1,9 @@
 // 1 - Crie um formulário com 4 botões que, ao clicar em cada um, altere a cor do plano de fundo da página para vermelho,
 // verde, azul e amarelo, respectivamente.
+const fundo = document.querySelector("body");
 
-function vermelho() {
-    document.body.style.backgroundColor = "red";
-}
+ function mudarCor(cor) {
+     fundo.style.backgroundColor = cor;
 
-function verde() {
-    document.body.style.backgroundColor = "green";
-}
+ }
 
-function azul() {
-    document.body.style.backgroundColor = "blue";
-}
-
-function amarelo() {
-    document.body.style.backgroundColor = "yellow";
-}
--- desafios/desafio-javascripto/exercicio02/exercicio02.js
+++ desafios/desafio-javascripto/exercicio02/exercicio02.js
@@ -1,2 +1,18 @@
 // 2 - Crie uma página com 4 links (tag <a>), cada um representando o nome de uma cor, que, utilizando o evento
 // onmouseover altere a cor do plano de fundo para cor correspondente
+
+function vermelho() {
+    document.body.style.backgroundColor = "red";
+}
+
+function verde() {
+    document.body.style.backgroundColor = "green";
+}
+
+function azul() {
+    document.body.style.backgroundColor = "blue";
+}
+
+function amarelo() {
+    document.body.style.backgroundColor = "yellow";
+}
