3374e90458b2a28cf1222ad4e89f69a02742d94c
Bruno Mota <bmotadev@gmail.com>
2023-05-17 16:30:59 -0300

    :recycle: refactor: exercicio 06 - refatorado

--- desafios/desafio-javascripto/exercicio06/exercicio06.js
+++ desafios/desafio-javascripto/exercicio06/exercicio06.js
@@ -1,2 +1,14 @@
 // 6 – Crie um parágrafo com um texto como conteúdo. Crie 4 botões. Dois para trocar a cor da fonte, de branco para
 // preto e vice-versa e outros dois para fazer o mesmo com o fundo do parágrafo. Use divs se julgar necessário.
+
+const texto = document.querySelector("p");
+
+function corDoTexto(cor) 
+{
+    texto.style.color = cor;
+}
+
+function corDeFundo(cor) 
+{
+    texto.style.backgroundColor = cor;
+}
