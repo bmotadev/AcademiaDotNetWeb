95d7048ea99cd05dbcc456721a9021f3d8c2cb62
Bruno Mota <bmotadev@gmail.com>
2023-05-17 17:03:25 -0300

    :recycle: refactor: exercicio 07 melhorado

--- desafios/desafio-javascripto/exercicio07/exercicio07.js
+++ desafios/desafio-javascripto/exercicio07/exercicio07.js
@@ -1,2 +1,16 @@
 // 7 – Crie um botão + (mais) e outro – (menos) na mesma página do exercício anterior. Esses botões aumentam e
 // diminuem o tamanho da fonte, respectivamente
+let texto = document.querySelector("p");
+let tamanhoDaFonte = 14;
+
+function mais()
+{
+    tamanhoDaFonte += 2;
+    texto.style.fontSize = tamanhoDaFonte + "px";
+}
+
+function menos()
+{
+    tamanhoDaFonte -= 2;
+    texto.style.fontSize  = tamanhoDaFonte + "px";
+}
