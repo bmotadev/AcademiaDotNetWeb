b777f2934621b17b548c9e81e4ff20c9f547cfbb
Bruno Mota <bmotadev@gmail.com>
2023-05-17 17:16:23 -0300

    :recycle: refactor: exercicio refatorado

--- desafios/desafio-javascripto/exercicio08/exercicio08.js
+++ desafios/desafio-javascripto/exercicio08/exercicio08.js
@@ -1,2 +1,10 @@
 // 8 – Ainda continuando no exercício 6, faça um botão que transforme todo o texto do parágrafo em maiúscula, e outro
 // em minúscula.
+
+let texto = document.querySelector("p");
+
+
+function formatoTexto(formato) 
+{
+    texto.style.textTransform = formato;
+}
