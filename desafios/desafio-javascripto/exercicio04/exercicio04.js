69a565109798d554b063176f2e2168dcc60cab58
Bruno Mota <bmotadev@gmail.com>
2023-05-17 15:17:52 -0300

    :recycle: refactor: exercicio 04 apimorado

--- desafios/desafio-javascripto/exercicio04/exercicio04.js
+++ desafios/desafio-javascripto/exercicio04/exercicio04.js
@@ -1,2 +1,16 @@
 // 4 – Crie um parágrafo (<p>) com um texto como conteúdo. Adicione um botão que, ao ser clicado, oculte ou mostre o
 // texto do parágrafo.
+
+function someAparece() 
+{
+    const texto = document.querySelector("p");
+
+    if(texto.style.display === "none") 
+    {
+        texto.style.display = "block";
+    }
+    else 
+    {
+        texto.style.display = "none";
+    }
+}
